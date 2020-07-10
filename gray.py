adb10da6c29d4e3f1ebf25114acea369b99c31e9
conda act pt <vscode@163.com>
2020-07-10 10:21:36 +0800

    fix for macos

--- gray.py
+++ gray.py
@@ -3,6 +3,9 @@
 #email 1349194985@qq.com
 
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("macOSX")
+from mpl_toolkits.mplot3d import Axes3D
 import cv2
 import numpy as np
 imgpath = "test.jpg"
@@ -14,9 +17,12 @@ cv2.imshow("cs1",img)
 img0 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.namedWindow("cs2")
 cv2.imshow("cs2",img0)
+
+
 sx = np.size(img0,0)
 sy = np.size(img0,1)
 print(sx,sy)
+print(np.shape(img0))
 print(type(img0))
 x = np.linspace(0, sx, sx)
 y = np.linspace(0, sy, sy)
@@ -25,6 +31,6 @@ Z = img0
 
 figure = plt.figure(1, figsize = (12, 4))
 subplot3d = plt.subplot(111, projection='3d')
-surface = subplot3d.plot_surface(X, Y, Z,rstride = 2, cstride = 2,cmap = plt.get_cmap('rainbow'))
+surface = subplot3d.plot_surface(X, Y, Z,rstride = 5, cstride = 5,cmap = plt.get_cmap('rainbow'))
 plt.show()
 cv2.waitKey(0)
